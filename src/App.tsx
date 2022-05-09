70d2a878c6a64a720a42d69892cd1814050a4657
eric.capiz <eric.capiz@nextechsol.com>
2022-05-09 14:56:12 -0500

    typescript setup w/boostrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mt-3'>
+      <div className="grid">
+        <div className="row">
+          <div className="col">
+            <p className="h3 fw-bold text-success">
+              Typescript App
+            </p>
+            <p className="fst-italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quod, illo non similique dolor explicabo culpa. Explicabo laudantium ex voluptatem neque ullam rem molestias, officiis placeat reiciendis iste nam dolor!</p>
+          <button className='btn btn-success btn-sm'>Learn More <i className='fa fa-book' /> </button>
+          </div>
+        </div>
+      </div>
+    
+    
     </div>
   );
 }
---
+++ src/components/Customer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+let Customer:React.FC = () => {
+    return(
+        <React.Fragment>
+            <h2>Customer</h2>
+        </React.Fragment>
+    )
+}
+
+export default Customer
